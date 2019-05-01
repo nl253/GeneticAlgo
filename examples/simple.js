8d2b37376bb933e93eeb65435ea7b17c1f3c044c
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-05-01 14:24:09 +0100

    Add perf metric for simple example

--- examples/simple.js
+++ examples/simple.js
@@ -8,13 +8,16 @@ const SEC = 1000;
 const fitness = xs => xs.reduce((x, y) => x + y, 0);
 
 const dtype = 'u32';  // search space of real numbers (floats) is more challenging (try 'f32')
-const nGenes = 1000;  // the more genes, the more difficult the task
+const nGenes = 500;  // the more genes, the more difficult the task
 
 const opts = { timeOutMS: 30 * SEC };
 
 const ga = new GA(fitness, nGenes, dtype, opts);
 
 const bestPossible = 2 ** 32 * nGenes; // every gene is max value of 32-bit unsigned int
+
+// [OPTIONAL] use the EventEmitter API for getting profiling
+ga.on('best', (_, fitness) => console.log('score', (fitness / bestPossible).toFixed(4), '/ 1.0'));
 
 /* ga.search() will create a generator that iterates over the best population
  * if you want the best candidate, just request the very first: */
