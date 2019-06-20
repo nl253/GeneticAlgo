8a85299b5e2434ae89f74d0d94de8a6f562c7989
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-06-20 10:43:20 +0100

    Fix multiobjective example

--- examples/multiobjective.ts
+++ examples/multiobjective.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-/**
- * This is a trivial example where the task is to maximise the value of each gene.
- */
-const GA = require('..');
+import { GeneticAlgorithm as  GA } from '../index';
+
 const SEC = 1000;
 
 const fitness = [
-  xs => xs.reduce((x, y) => x - y, 0),             // ensure all max
+// @ts-ignore
+  xs => xs.reduce((x, y) => x - y, 0), // ensure all max
+// @ts-ignore
   xs => xs.map(x => x % 2 === 0 ? 1 : 0).reduce((x1, x2) => x1 + x2, 0), // ensure all even
+// @ts-ignore
   xs => xs.map(x => x % 3 === 0 ? 1 : 0).reduce((x1, x2) => x1 + x2, 0), // ensure all multiples of 3
 ];
 
@@ -19,16 +20,10 @@ const opts = {
   // to make it very verbose...
   // log: 2,
   // to make it verbose...
-  debug: 1,
-  emitFittest: true,
+  logLvl: 1,
 };
 
 const ga = new GA(fitness, nGenes, dtype, opts);
-
-// [OPTIONAL] use the EventEmitter API for getting profiling
-ga.on('score', env => console.log('score', env.bestScore));
-// when opts.emitFittest = true, you can print best on each round
-ga.on('score', env => console.log('[', env.best.join(','), ']'));
 
 /* ga.search() will create a generator that iterates over the best population
  * if you want the best candidate, just request the very first: */
