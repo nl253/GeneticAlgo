569097f21ae1ee2a587007806ddd7b360c146017
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-09-14 20:43:30 +0100

    Extract MIN_PERF, rename

--- index.test.js
+++ index.test.js
@@ -8,6 +8,7 @@ const DEFAULT_EVENT = 'round';
 const DEFAULT_DELAY = 300;
 const NGENES = 100;
 const DEFAULT_POPSIZE = 200;
+const MIN_PERFORMANCE = 0.85;
 
 const DATATYPES = [
   'u8',
@@ -37,7 +38,7 @@ const FUNCTS = [
  * @param {(Function|Function[])[]} fitnessFuncts
  * @param {Number} duration
  */
-function simulateAll(
+function simulate(
   checks = [],
   opts = {},
   event = DEFAULT_EVENT,
@@ -59,41 +60,41 @@ function simulateAll(
 }
 
 test('search is a public method on GeneticAlgorithm', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('search'),
   ]);
 });
 
 test('rIdx INT is defined (and public) during runtime', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('rIdx'),
     ga => expect(Number.isInteger(ga.rIdx)).toBeTruthy(),
   ]);
 });
 
 test('op ("mutate" or "crossover") is defined (and public) during runtime after either occurrs', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('op'),
     ga => expect(ga.op).toMatch(/^(mutate|crossover)$/),
   ], {}, 'op');
 });
 
 test('rank INT is defined (and public) during runtime', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('rank'),
     ga => expect(Number.isInteger(ga.rank)).toBeTruthy(),
   ]);
 });
 
 test('cIdx is defined (and public) during runtime', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('cIdx'),
     ga => expect(Number.isInteger(ga.cIdx)).toBeTruthy(),
   ]);
 });
 
 test('pop TYPED ARRAY is valid and defined (and public) when starting', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('pop'),
     ga => expect(ga.pop).toHaveProperty('length'),
     ga => expect(ga.pop.length > 0).toBeTruthy(),
@@ -106,7 +107,7 @@ test('pop TYPED ARRAY is valid and defined (and public) when starting', () => {
 });
 
 test('idxs TYPED ARRAY is valid and defined (and public) when starting', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('idxs'),
     ga => expect(ga.idxs).toHaveProperty('length'),
     ga => expect(ga.idxs.length > 0).toBeTruthy(),
@@ -119,7 +120,7 @@ test('idxs TYPED ARRAY is valid and defined (and public) when starting', () => {
 });
 
 test('bestScores TYPED ARRAY is valid and defined (and public) when starting', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('bestScores'),
     ga => expect(ga.bestScores).toHaveProperty('length'),
     ga => expect(ga.bestScores.length > 0).toBeTruthy(),
@@ -132,7 +133,7 @@ test('bestScores TYPED ARRAY is valid and defined (and public) when starting', (
 });
 
 test('scores TYPED ARRAY is valid and defined (and public) when starting', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('scores'),
     ga => expect(ga.scores).toHaveProperty('length'),
     ga => expect(ga.scores.length > 0).toBeTruthy(),
@@ -145,14 +146,14 @@ test('scores TYPED ARRAY is valid and defined (and public) when starting', () =>
 });
 
 test('start time (startTm INT) is defined and public during runtime', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('startTm'),
     ga => expect(Number.isInteger(ga.startTm)).toBeTruthy(),
   ], {}, 'start');
 });
 
 test('scores are improving with time', () => {
-  simulateAll([
+  simulate([
     ga => {
       const snaphshot1 = ga.scores;
       setTimeout(() => {
@@ -166,7 +167,7 @@ test('scores are improving with time', () => {
 });
 
 test('time and % done are advancing', () => {
-  simulateAll([
+  simulate([
     ga => {
       const pDone1 = ga.percentageDone;
       setTimeout(() => expect(ga.percentageDone).toBeGreaterThan(pDone1), DEFAULT_DELAY);
@@ -191,7 +192,7 @@ test('time and % done are advancing', () => {
 });
 
 test('best candidate (0th best) is accessible and is not empty', () => {
-  simulateAll([
+  simulate([
     ga => expect(ga).toHaveProperty('bestCand'),
     ga => expect(ga.bestCand).toHaveProperty('length'),
     ga => expect(ga.bestCand.length > 0).toBeTruthy(),
@@ -199,7 +200,6 @@ test('best candidate (0th best) is accessible and is not empty', () => {
     ga => expect(ga.nthBestCand(0)).toStrictEqual(ga.bestCand),
   ]);
 });
-
 
 describe('ga does well on simple problems', () => {
   const f = (cand) => cand.reduce((x, y) => x + y, 0);
@@ -209,8 +209,8 @@ describe('ga does well on simple problems', () => {
       const dtype = `${dt}${nBits}`;
       const timeSec = 5;
       test(`dtype = ${dtype}, best possible = ${bestPossible} (${timeSec} sec)`, () => {
-        simulateAll(
-          [ga => expect(f(ga.bestCand) / bestPossible).toBeGreaterThan(0.85)],
+        simulate(
+          [ga => expect(f(ga.bestCand) / bestPossible).toBeGreaterThan(MIN_PERFORMANCE)],
           { timeOutMS: Duration.seconds(timeSec) },
           'end',
           [dtype],
